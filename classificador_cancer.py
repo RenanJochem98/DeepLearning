bb5a5dc70b8f2d14ffee308d61920860c7334b65
Renan <renanjochem98@gmail.com>
2020-04-27 11:26:37 -0300

    Criando primeiro classificador simples, baseado no keras

---
+++ classificador_cancer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import keras
+
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense # siginifica que todas as camadas  estao ligadas entre si
+
+entradas_file_name = "datasets/breast_cancer/entradas.csv"
+saidas_file_name = "datasets/breast_cancer/saidas.csv"
+
+entradas = pd.read_csv(entradas_file_name)
+saidas = pd.read_csv(saidas_file_name)
+
+entradas_treino, entradas_teste, saidas_treino, saidas_teste = train_test_split(entradas, saidas, test_size=0.25)
+
+classificador = Sequential()
+classificador.add(Dense(units=16, activation='relu', kernel_initializer='random_uniform', input_dim=30))
+classificador.add(Dense(units=1, activation='sigmoid'))
+
+classificador.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
+
+classificador.fit(entradas_treino, saidas_treino, epochs=1000)
